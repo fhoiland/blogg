857e5790c7053f6b998aad37c398d9ae5df36ba8
freddiien <freddiihd@gmail.com>
2022-07-15 21:34:44 +0200

    testing .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,20 @@
+
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-    
-    eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy("admin");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
-    
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    };
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('admin');
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
 }
